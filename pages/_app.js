6c2caee9c84d0d79c378a5459027bdb5045d2065
Kien Dang <ikien.dang@gmail.com>
2023-12-17 16:06:26 -0500

    add snowfall

--- pages/_app.js
+++ pages/_app.js
@@ -7,26 +7,50 @@ import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
+import Snowfall from 'react-snowfall'
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { useRouter } from 'next/router'
+import { useTheme } from 'next-themes'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+const isDecemberOrJanuary = [11, 0].includes(new Date().getMonth())
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  const currentPathname = router.pathname
+
+  const shouldSnow =
+    ['/', '/blog', '/tags', '/about'].includes(currentPathname) && isDecemberOrJanuary
+
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+    <>
+      {shouldSnow ? (
+        <Snowfall
+          style={{
+            position: 'fixed',
+            width: '100vw',
+            height: '100vh',
+          }}
+          color="white"
+          snowflakeCount={28}
+        />
+      ) : null}
+
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
+    </>
   )
 }
